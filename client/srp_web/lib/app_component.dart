eeb12efb025fc8ac7cd3aea24f8d952e92eb8440
Didiet Noor <lynxluna@gmail.com>
2018-11-04 23:46:27 +0700

    Add login screen dart

---
+++ client/srp_web/lib/app_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'main',
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+)
+class AppComponent {
+  var name = 'Angular';
+}
